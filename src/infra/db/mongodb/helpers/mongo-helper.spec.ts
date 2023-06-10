07c17169c9834318e62455cb9a03797d68961737
mlucasdev <lucasmsantos819@gmail.com>
2023-06-10 14:13:39 -0300

    test: ensure connect to the specified URL

---
+++ src/infra/db/mongodb/helpers/mongo-helper.spec.ts
@@ -0,0 +1,14 @@
+import { MongoHelper as sut } from './mongo-helper'
+import { MongoClient } from 'mongodb'
+
+describe('Mongo Helper', () => {
+  test('Should connect to the specified URL', async () => {
+    const url = 'mongodb://127.0.0.1:27017/db-test'
+    const mongoClientMock = {
+      connect: jest.fn().mockResolvedValue(true)
+    }
+    MongoClient.connect = jest.fn().mockResolvedValue(mongoClientMock)
+    sut.connect(url)
+    expect(MongoClient.connect).toHaveBeenCalledWith(url)
+  })
+})
