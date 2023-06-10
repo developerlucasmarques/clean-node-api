92fa3b162d41e07b841bf1cf48c75414f77fca04
mlucasdev <lucasmsantos819@gmail.com>
2023-06-10 14:18:07 -0300

    test: ensure Mongo Helper connect to the default URL if no URL provided

--- src/infra/db/mongodb/helpers/mongo-helper.spec.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.spec.ts
@@ -11,4 +11,14 @@ describe('Mongo Helper', () => {
     sut.connect(url)
     expect(MongoClient.connect).toHaveBeenCalledWith(url)
   })
+
+  test('Should connect to the default URL if no URL provided', async () => {
+    const defaultUrl = 'mongodb://127.0.0.1:27017/clean-node-api'
+    const mongoClientMock = {
+      connect: jest.fn().mockResolvedValue(true)
+    }
+    MongoClient.connect = jest.fn().mockResolvedValue(mongoClientMock)
+    sut.connect(undefined)
+    expect(MongoClient.connect).toHaveBeenCalledWith(defaultUrl)
+  })
 })
