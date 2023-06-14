1aa69b2b8b7d2621d1f79c64c3de2bde3cde8c1e
mlucasdev <lucasmsantos819@gmail.com>
2023-06-14 13:01:05 -0300

    feat: ensure LoginController returns 400 if an invalid email is provided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,4 +1,4 @@
-import { MissingParamError } from '../../errors'
+import { InvalidParamError, MissingParamError } from '../../errors'
 import { badRequest, ok } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 import { EmailValidator } from '../signup/signup-protocols'
@@ -13,7 +13,10 @@ export class LoginController implements Controller {
         return badRequest(new MissingParamError(field))
       }
     }
-    this.emailValidator.isValid(httpRequest.body.email)
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+    if (!isValid) {
+      return badRequest(new InvalidParamError('email'))
+    }
     return await Promise.resolve(ok(''))
   }
 }
