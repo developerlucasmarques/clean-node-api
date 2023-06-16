562fbd33c00fc2500573bf7dd5c82363a5ad37b0
mlucasdev <lucasmsantos819@gmail.com>
2023-06-16 19:42:06 -0300

    test: ensure return InvalidNameError if name contaisn special character

--- src/domain/account/name.spec.ts
+++ src/domain/account/name.spec.ts
@@ -2,24 +2,29 @@ import { InvalidNameError } from '../errors/invalid-name-error'
 import { Name } from './name'
 
 describe('Name Value Object', () => {
-  test('Should return InvalidNameError if length of name is less than 3 caracters', () => {
+  test('Should return InvalidNameError if length of name is less than 3 characters', () => {
     const sut = Name.create('ab')
     expect(sut.value).toEqual(new InvalidNameError('ab'))
   })
 
-  test('Should return InvalidNameError if length of name is greater than 50 caracters', () => {
-    const name = 'any_name_any_name_any_name_any_name_any_name_any_na'
+  test('Should return InvalidNameError if length of name is greater than 50 characters', () => {
+    const name = 'any name any name any name any name any name any na'
     const sut = Name.create(name)
     expect(sut.value).toEqual(new InvalidNameError(name))
   })
 
   test('Should remove the spaces at the beginning and at the end of the name', () => {
-    const sut = Name.create(' any_name ')
-    expect(sut.value).toEqual({ name: 'any_name' })
+    const sut = Name.create(' any name ')
+    expect(sut.value).toEqual({ name: 'any name' })
   })
 
   test('Should return InvalidNameError if name contains number', () => {
-    const sut = Name.create('any_0_name')
-    expect(sut.value).toEqual(new InvalidNameError('any_0_name'))
+    const sut = Name.create('any 0 name')
+    expect(sut.value).toEqual(new InvalidNameError('any 0 name'))
+  })
+
+  test('Should return InvalidNameError if name contains special character', () => {
+    const sut = Name.create('invalid_name')
+    expect(sut.value).toEqual(new InvalidNameError('invalid_name'))
   })
 })
