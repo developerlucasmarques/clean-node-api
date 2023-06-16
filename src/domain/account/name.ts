1ae5c9469e6e9c9755131faaa3ff2c2f8338015a
mlucasdev <lucasmsantos819@gmail.com>
2023-06-16 19:41:58 -0300

    feat: ensure return InvalidNameError if name contaisn special character

--- src/domain/account/name.ts
+++ src/domain/account/name.ts
@@ -18,8 +18,8 @@ export class Name {
     if (!name || name.length < 3 || name.length > 50) {
       return false
     }
-    const containNumbersRegex = /[0-9]/g
-    if (containNumbersRegex.test(name)) {
+    const regex = /^.*[!@#$%^&*(),.?":{}|<>0-9_].*$/
+    if (regex.test(name)) {
       return false
     }
     return true
