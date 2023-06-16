31c4ae8ba69460bc91dea52d26d27ab260e0a6c5
mlucasdev <lucasmsantos819@gmail.com>
2023-06-16 19:12:43 -0300

    refactor: testing only the return value of Name

--- src/domain/account/name.spec.ts
+++ src/domain/account/name.spec.ts
@@ -1,17 +1,16 @@
-import { left } from '../../shared/either'
 import { InvalidNameError } from '../errors/invalid-name-error'
 import { Name } from './name'
 
 describe('Name Value Object', () => {
   test('Should return InvalidNameError if length of name is less than 3 caracters', () => {
     const sut = Name.create('ab')
-    expect(sut).toEqual(left(new InvalidNameError('ab')))
+    expect(sut.value).toEqual(new InvalidNameError('ab'))
   })
 
   test('Should return InvalidNameError if length of name is greater than 50 caracters', () => {
     const name = 'any_name_any_name_any_name_any_name_any_name_any_na'
     const sut = Name.create(name)
-    expect(sut).toEqual(left(new InvalidNameError(name)))
+    expect(sut.value).toEqual(new InvalidNameError(name))
   })
 
   test('Should remove the spaces at the beginning and at the end of the name', () => {
