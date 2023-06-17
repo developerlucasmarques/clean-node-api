544e6545c60ac1f6087a612a59dec03e0c6bd852
mlucasdev <lucasmsantos819@gmail.com>
2023-06-17 20:38:04 -0300

    feat: ensure an Account is created if the values are valid

--- src/domain/account/account.ts
+++ src/domain/account/account.ts
@@ -1,18 +1,20 @@
-import { Either, left, right } from '../../shared/either'
-import { InvalidNameError } from '../errors/invalid-name-error'
+import { left, right } from '../../shared/either'
 import { AddAccountModel } from '../usecases/add-account'
+import { AccountResponse } from './account-response'
 import { Email } from './value-objects/email'
 import { Name } from './value-objects/name'
 import { Password } from './value-objects/password'
 
 export class Account {
   private constructor (
-    private readonly name: Name
+    private readonly name: Name,
+    private readonly email: Email,
+    private readonly password: Password
   ) {
     Object.freeze(this)
   }
 
-  static create (accountData: AddAccountModel): Either<InvalidNameError, Account> {
+  static create (accountData: AddAccountModel): AccountResponse {
     const nameOrError = Name.create(accountData.name)
     const emailOrError = Email.create(accountData.email)
     const passwordOrError = Password.create(accountData.password)
@@ -26,6 +28,8 @@ export class Account {
       return left(passwordOrError.value)
     }
     const name = nameOrError.value
-    return right(new Account(name))
+    const email = emailOrError.value
+    const password = passwordOrError.value
+    return right(new Account(name, email, password))
   }
 }
