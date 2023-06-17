713efc49051bc3b99538f68c9fd3dfcca4b49e4b
mlucasdev <lucasmsantos819@gmail.com>
2023-06-17 20:15:50 -0300

    test: ensure Account call Email with correct value

--- src/domain/account/account.spec.ts
+++ src/domain/account/account.spec.ts
@@ -1,20 +1,34 @@
 import { left } from '../../shared/either'
 import { InvalidNameError } from '../errors/invalid-name-error'
+import { AddAccountModel } from '../usecases/add-account'
 import { Account } from './account'
+import { Email } from './value-objects/email'
 import { Name } from './value-objects/name'
+
+const makeFakeAccountData = (): AddAccountModel => ({
+  name: 'any name',
+  email: 'any_email@mail.com',
+  password: 'password1234'
+})
 
 describe('Account', () => {
   test('Should call Name with correct values', () => {
-    const createSpy = jest.spyOn(Name, 'create')
-    Account.create('any name')
-    expect(createSpy).toHaveBeenCalledWith('any name')
+    const createNameSpy = jest.spyOn(Name, 'create')
+    Account.create(makeFakeAccountData())
+    expect(createNameSpy).toHaveBeenCalledWith('any name')
   })
 
   test('Should return InvalidNameError if Name return InvalidNameError', () => {
     jest.spyOn(Name, 'create').mockReturnValueOnce(
       left(new InvalidNameError('invalid name'))
     )
-    const sut = Account.create('invalid name')
+    const sut = Account.create(makeFakeAccountData())
     expect(sut.value).toEqual(new InvalidNameError('invalid name'))
+  })
+
+  test('Should call Email with correct value', () => {
+    const createEmailSpy = jest.spyOn(Email, 'create')
+    Account.create(makeFakeAccountData())
+    expect(createEmailSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
 })
