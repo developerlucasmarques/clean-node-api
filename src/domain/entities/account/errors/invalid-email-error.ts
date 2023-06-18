2224b59034518ba9806fa3b60ecb884cb62ad22a
mlucasdev <lucasmsantos819@gmail.com>
2023-06-18 15:06:59 -0300

    refator: error message change from double quote to single quote

--- src/domain/entities/account/errors/invalid-email-error.ts
+++ src/domain/entities/account/errors/invalid-email-error.ts
@@ -1,6 +1,6 @@
 export class InvalidEmailError extends Error {
   constructor (email: string) {
-    super(`The email "${email}" is invalid`)
+    super(`The email '${email}' is invalid`)
     this.name = 'InvalidEmailError'
   }
 }
--- src/domain/entities/account/errors/invalid-name-error.ts
+++ src/domain/entities/account/errors/invalid-name-error.ts
@@ -1,6 +1,6 @@
 export class InvalidNameError extends Error {
   constructor (name: string) {
-    super(`The name "${name}" is invalid`)
+    super(`The name '${name}' is invalid`)
     this.name = 'InvalidNameError'
   }
 }
