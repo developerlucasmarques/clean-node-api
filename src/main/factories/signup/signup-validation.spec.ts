4e4165d5e89eb0b9f23fcd5514a4a707553b3b04
mlucasdev <lucasmsantos819@gmail.com>
2023-06-21 20:04:37 -0300

    refactor: move signup factory to your folder

--- src/main/factories/signup/signup-validation.spec.ts
+++ src/main/factories/signup/signup-validation.spec.ts
@@ -1,10 +1,10 @@
-import { Validation } from '../../presentation/controllers/signup'
-import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validation'
-import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation'
-import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
+import { Validation } from '../../../presentation/controllers/signup'
+import { CompareFieldsValidation } from '../../../presentation/helpers/validators/compare-fields-validation'
+import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-field-validation'
+import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
 import { makeSignUpValidation } from './signup-validation'
 
-jest.mock('../../presentation/helpers/validators/validation-composite')
+jest.mock('../../../presentation/helpers/validators/validation-composite')
 
 describe('SignUpValidation Factory', () => {
   test('Should call ValidationComposite with all validations', () => {
