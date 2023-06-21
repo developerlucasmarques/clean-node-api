b09958ca634fa158eb23fd993a8043589e392f76
mlucasdev <lucasmsantos819@gmail.com>
2023-06-21 17:46:38 -0300

    test: ensure CompareFieldsValidation return null if comparation succeeds

--- src/presentation/helpers/validators/compare-fields-validation.spec.ts
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -2,7 +2,7 @@ import { InvalidParamError } from '../../errors'
 import { CompareFieldsValidation } from './compare-fields-validation'
 
 describe('CompareFields Validation', () => {
-  test('Should return a InvalidaParamError if comparation fails', () => {
+  test('Should return a InvalidaParamError if the comparation fails', () => {
     const sut = new CompareFieldsValidation('fieldName', 'fieldToCompareName')
     const result = sut.validate({
       name: 'any name',
@@ -10,5 +10,15 @@ describe('CompareFields Validation', () => {
       fieldToCompareName: 'invalid field'
     })
     expect(result.value).toEqual(new InvalidParamError('fieldToCompareName'))
+  })
+
+  test('Should return null if compartion succeeds', () => {
+    const sut = new CompareFieldsValidation('fieldName', 'fieldToCompareName')
+    const result = sut.validate({
+      name: 'any name',
+      fieldName: 'any field',
+      fieldToCompareName: 'any field'
+    })
+    expect(result.value).toBe(null)
   })
 })
