0fc2939deb18e97969029d526c7ed2c6c1e37993
mlucasdev <lucasmsantos819@gmail.com>
2023-06-22 18:23:22 -0300

    feat: ensure DbAuthentication return AuthenticationError if LoadAccountByEmailRepository returns error

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,12 +1,16 @@
+import { AuthenticationError } from '../../../domain/errors/authentication-error'
 import { Authentication, AuthenticationData, AuthenticationResponse } from '../../../domain/usecases/authentication'
-import { right } from '../../../shared/either'
+import { left, right } from '../../../shared/either'
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
 
 export class DbAuthentication implements Authentication {
   constructor (private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository) {}
 
   async auth (authenticationData: AuthenticationData): Promise<AuthenticationResponse> {
-    await this.loadAccountByEmailRepository.load(authenticationData.email)
+    const accountOrError = await this.loadAccountByEmailRepository.load(authenticationData.email)
+    if (accountOrError.isLeft()) {
+      return left(new AuthenticationError())
+    }
     return right('access_token')
   }
 }
