55f07e03da23382d1e98d00531a9a0e67da9708b
mlucasdev <lucasmsantos819@gmail.com>
2023-06-22 21:22:01 -0300

    feat: ensure DbAuthentication call TokenGenerator with correct id

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -2,12 +2,14 @@ import { AuthenticationError } from '../../../domain/errors/authentication-error
 import { Authentication, AuthenticationData, AuthenticationResponse } from '../../../domain/usecases/authentication'
 import { left, right } from '../../../shared/either'
 import { HashComparer } from '../../protocols/criptography/hash-comparer'
+import { TokenGenerator } from '../../protocols/criptography/token-generator'
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
 
 export class DbAuthentication implements Authentication {
   constructor (
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
-    private readonly hashComparer: HashComparer
+    private readonly hashComparer: HashComparer,
+    private readonly tokenGenerator: TokenGenerator
   ) {}
 
   async auth (authenticationData: AuthenticationData): Promise<AuthenticationResponse> {
@@ -23,6 +25,7 @@ export class DbAuthentication implements Authentication {
     if (!comparerResult) {
       return left(new AuthenticationError())
     }
+    this.tokenGenerator.generate(accountOrError.value.id)
     return right('access_token')
   }
 }
