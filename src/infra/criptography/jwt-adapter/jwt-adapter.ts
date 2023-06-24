28297c57a80380066035eda0787fdd0678b72ac6
mlucasdev <lucasmsantos819@gmail.com>
2023-06-23 22:02:13 -0300

    feat: ensure JwtAdapter return a token if sign success

--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -5,7 +5,7 @@ export class JwtAdapter implements Encrypter {
   constructor (private readonly secretKey: string) {}
 
   async encrypt (value: string): Promise<string> {
-    jwt.sign({ id: value }, this.secretKey, { expiresIn: '24h' })
-    return await Promise.resolve('string')
+    const token = jwt.sign({ id: value }, this.secretKey, { expiresIn: '24h' })
+    return token
   }
 }
