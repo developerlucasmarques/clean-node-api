1587e18750648cbc09fcf1f551a8b69f6cd0932f
mlucasdev <lucasmsantos819@gmail.com>
2023-06-24 18:53:43 -0300

    feat: ensure AccountMongoRepository return error if loadAccountByEmail not found account

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,6 +1,6 @@
-import { AccountData, AccountModel, AddAccountRepository } from '.'
-import { LoadAccountByEmailRepository, LoadAccountByEmailResponse } from '../../../../data/usecases/authentication'
-import { right } from '../../../../shared/either'
+import { AddAccountRepository, AccountModel, AccountData } from '.'
+import { LoadAccountByEmailError, LoadAccountByEmailRepository, LoadAccountByEmailResponse } from '../../../../data/usecases/authentication'
+import { left, right } from '../../../../shared/either'
 import { MongoHelper } from '../helpers/mongo-helper'
 
 export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
@@ -13,6 +13,9 @@ export class AccountMongoRepository implements AddAccountRepository, LoadAccount
   async loadAccountByEmail (email: string): Promise<LoadAccountByEmailResponse> {
     const accountCollection = await MongoHelper.getCollection('account')
     const account = await accountCollection.findOne({ email })
+    if (!account) {
+      return left(new LoadAccountByEmailError(email))
+    }
     return right(MongoHelper.map(account))
   }
 }
