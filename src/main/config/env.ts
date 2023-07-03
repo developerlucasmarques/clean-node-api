b4e81cbce70ce9547f6807823e86c895d0afd6b5
mlucasdev <lucasmsantos819@gmail.com>
2023-07-03 16:50:20 -0300

    chore: config dotenv

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 export default {
   mongoUrl: process.env.MONGO_URL ?? 'mongodb://127.0.0.1:27017/clean-node-api',
   port: process.env.PORT ?? 5050,
