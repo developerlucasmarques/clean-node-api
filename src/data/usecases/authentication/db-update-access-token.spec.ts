38655a61e3eab562cb40e8ff18d6b4d180f5aef5
mlucasdev <lucasmsantos819@gmail.com>
2023-07-04 16:32:14 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/authentication/db-update-access-token.spec.ts
+++ src/data/usecases/authentication/db-update-access-token.spec.ts
@@ -1,14 +1,31 @@
 import { Encrypter, DbUpdateAccessToken } from '.'
 
+const makeEncrypterStub = (): Encrypter => {
+  class EncrypterStub implements Encrypter {
+    async encrypt (value: string): Promise<string> {
+      return await Promise.resolve('any_token')
+    }
+  }
+  return new EncrypterStub()
+}
+
+interface SutTypes {
+  sut: DbUpdateAccessToken
+  encrypterStub: Encrypter
+}
+
+const makeSut = (): SutTypes => {
+  const encrypterStub = makeEncrypterStub()
+  const sut = new DbUpdateAccessToken(encrypterStub)
+  return {
+    sut,
+    encrypterStub
+  }
+}
+
 describe('UpdateAccessToken UseCase', () => {
   test('Should call Encrypter with correct account id', async () => {
-    class EncrypterStub implements Encrypter {
-      async encrypt (value: string): Promise<string> {
-        return await Promise.resolve('any_token')
-      }
-    }
-    const encrypterStub = new EncrypterStub()
-    const sut = new DbUpdateAccessToken(encrypterStub)
+    const { sut, encrypterStub } = makeSut()
     const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
     sut.update('account_id')
     expect(encryptSpy).toHaveBeenCalledWith('account_id')
