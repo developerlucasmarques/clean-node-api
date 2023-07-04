cd5808b02f1fc5968ad253d8f921cef4756a9a0b
mlucasdev <lucasmsantos819@gmail.com>
2023-07-04 16:45:38 -0300

    test: ensure DbUpdateAccessToken calls UpdateAccessTokenRepository with correct values

--- src/data/usecases/authentication/db-update-access-token.spec.ts
+++ src/data/usecases/authentication/db-update-access-token.spec.ts
@@ -1,4 +1,4 @@
-import { Encrypter, DbUpdateAccessToken } from '.'
+import { Encrypter, DbUpdateAccessToken, UpdateAccessTokenRepository, UpdateAccessTokenData } from '.'
 
 const makeEncrypterStub = (): Encrypter => {
   class EncrypterStub implements Encrypter {
@@ -9,17 +9,29 @@ const makeEncrypterStub = (): Encrypter => {
   return new EncrypterStub()
 }
 
+const makeUpdateAccessTokenRepositoryStub = (): UpdateAccessTokenRepository => {
+  class UpdateAccessTokenRepositoryStub implements UpdateAccessTokenRepository {
+    async updateAccessToken (values: UpdateAccessTokenData): Promise<void> {
+      Promise.resolve()
+    }
+  }
+  return new UpdateAccessTokenRepositoryStub()
+}
+
 interface SutTypes {
   sut: DbUpdateAccessToken
   encrypterStub: Encrypter
+  updateAccessTokenRepositoryStub: UpdateAccessTokenRepository
 }
 
 const makeSut = (): SutTypes => {
   const encrypterStub = makeEncrypterStub()
-  const sut = new DbUpdateAccessToken(encrypterStub)
+  const updateAccessTokenRepositoryStub = makeUpdateAccessTokenRepositoryStub()
+  const sut = new DbUpdateAccessToken(encrypterStub, updateAccessTokenRepositoryStub)
   return {
     sut,
-    encrypterStub
+    encrypterStub,
+    updateAccessTokenRepositoryStub
   }
 }
 
@@ -38,5 +50,15 @@ describe('UpdateAccessToken UseCase', () => {
     })
     const promise = sut.update('account_id')
     await expect(promise).rejects.toThrow(new Error())
+  })
+
+  test('Should call UpdateAccessTokenRepository with correct values', async () => {
+    const { sut, updateAccessTokenRepositoryStub } = makeSut()
+    const updateAccessTokenSpy = jest.spyOn(updateAccessTokenRepositoryStub, 'updateAccessToken')
+    await sut.update('account_id')
+    expect(updateAccessTokenSpy).toHaveBeenCalledWith({
+      accountId: 'account_id',
+      accessToken: 'any_token'
+    })
   })
 })
