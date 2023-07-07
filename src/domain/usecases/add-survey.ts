12c000a06dd326e333888b6e9bfa3bf6ca4de75d
mlucasdev <lucasmsantos819@gmail.com>
2023-07-07 17:15:13 -0300

    feat: ensure AddSurveyController call AddSurvey with correct values

---
+++ src/domain/usecases/add-survey.ts
@@ -0,0 +1,13 @@
+export interface SurveyAnswer {
+  image: string
+  answer: string
+}
+
+export interface AddSurveyData {
+  question: string
+  answers: SurveyAnswer[]
+}
+
+export interface AddSurvey {
+  add: (data: AddSurveyData) => Promise<void>
+}
--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,13 +1,17 @@
-import { Controller, HttpRequest, HttpResponse, Validation, badRequest } from '.'
+import { AddSurvey, Controller, HttpRequest, HttpResponse, Validation, badRequest } from '.'
 
 export class AddSurveyController implements Controller {
-  constructor (private readonly validation: Validation) {}
+  constructor (
+    private readonly validation: Validation,
+    private readonly addSurvey: AddSurvey
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const validationResult = this.validation.validate(httpRequest.body)
     if (validationResult.isLeft()) {
       return badRequest(validationResult.value)
     }
+    await this.addSurvey.add(httpRequest.body)
     return await Promise.resolve({
       statusCode: 1,
       body: 'any_body'
