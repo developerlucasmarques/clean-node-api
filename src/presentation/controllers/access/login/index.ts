f2360dc6b2b33fa573f01a27eef794b835741790
mlucasdev <lucasmsantos819@gmail.com>
2023-07-07 16:21:28 -0300

    refactor: move singup and login controllers to a access folder

---
+++ src/presentation/controllers/access/login/index.ts
@@ -0,0 +1,4 @@
+export * from '../../../protocols'
+export * from '../../../../validation/protocols/email-validator'
+export * from '../../../../domain/usecases/authentication'
+export * from '../../../../domain/errors/authentication-error'
