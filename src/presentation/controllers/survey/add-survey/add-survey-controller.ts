c76c00c7cbe48ca45e9eaf5b54090a4f28be31c9
mlucasdev <lucasmsantos819@gmail.com>
2023-07-07 16:52:27 -0300

    feat: ensure AddSurveyController return 404 if Validation fails

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,10 +1,13 @@
-import { Controller, HttpRequest, HttpResponse, Validation } from '.'
+import { Controller, HttpRequest, HttpResponse, Validation, badRequest } from '.'
 
 export class AddSurveyController implements Controller {
   constructor (private readonly validation: Validation) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.validation.validate(httpRequest.body)
+    const validationResult = this.validation.validate(httpRequest.body)
+    if (validationResult.isLeft()) {
+      return badRequest(validationResult.value)
+    }
     return await Promise.resolve({
       statusCode: 1,
       body: 'any_body'
