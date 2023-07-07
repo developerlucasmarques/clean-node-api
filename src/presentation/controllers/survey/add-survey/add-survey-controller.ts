eec2f9276df2b62e63977409e0fa2a2fb3421941
mlucasdev <lucasmsantos819@gmail.com>
2023-07-07 17:51:26 -0300

    feat: AddSurveyController return 500 if AddSurvey throws

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,4 +1,4 @@
-import { AddSurvey, Controller, HttpRequest, HttpResponse, Validation, badRequest } from '.'
+import { AddSurvey, Controller, HttpRequest, HttpResponse, Validation, badRequest, serverError } from '.'
 
 export class AddSurveyController implements Controller {
   constructor (
@@ -7,15 +7,19 @@ export class AddSurveyController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const validationResult = this.validation.validate(httpRequest.body)
-    if (validationResult.isLeft()) {
-      return badRequest(validationResult.value)
+    try {
+      const validationResult = this.validation.validate(httpRequest.body)
+      if (validationResult.isLeft()) {
+        return badRequest(validationResult.value)
+      }
+      const { question, answers } = httpRequest.body
+      await this.addSurvey.add({ question, answers })
+      return await Promise.resolve({
+        statusCode: 1,
+        body: 'any_body'
+      })
+    } catch (error: any) {
+      return serverError(error)
     }
-    const { question, answers } = httpRequest.body
-    await this.addSurvey.add({ question, answers })
-    return await Promise.resolve({
-      statusCode: 1,
-      body: 'any_body'
-    })
   }
 }
