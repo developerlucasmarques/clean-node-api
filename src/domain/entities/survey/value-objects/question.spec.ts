cf5cfccab4c242eaa80cdd4ebdffbe3c470ecdf4
mlucasdev <lucasmsantos819@gmail.com>
2023-07-29 09:59:14 -0300

    test: ensure Question return InvalidQuestionError if question contains more than 300 characters

--- src/domain/entities/survey/value-objects/question.spec.ts
+++ src/domain/entities/survey/value-objects/question.spec.ts
@@ -5,11 +5,17 @@ import { Question } from './question'
 describe('Question Value Object', () => {
   test('Should return InvalidQuestionError if question not provided', () => {
     const sut = Question.create('')
-    expect(sut).toEqual(left(new InvalidQuestionError('Question not provided')))
+    expect(sut).toEqual(left(new InvalidQuestionError('not provided')))
   })
 
   test('Should return InvalidQuestionError if question contains less than 7 characters', () => {
     const sut = Question.create('1234')
-    expect(sut).toEqual(left(new InvalidQuestionError('Contains less than 7 characters')))
+    expect(sut).toEqual(left(new InvalidQuestionError('contains less than 7 characters')))
+  })
+
+  test('Should return InvalidQuestionError if question contains more than 300 characters', () => {
+    const question = 'A'.repeat(301)
+    const sut = Question.create(question)
+    expect(sut).toEqual(left(new InvalidQuestionError('contains more than 300 characters')))
   })
 })
