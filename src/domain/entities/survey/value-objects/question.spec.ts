c0dbdf4d4406f95e806a5b01f87db30ccd5e3b3a
mlucasdev <lucasmsantos819@gmail.com>
2023-07-29 09:54:57 -0300

    test: ensure Question return InvalidQuestionError if question contains less than 7 characteres

--- src/domain/entities/survey/value-objects/question.spec.ts
+++ src/domain/entities/survey/value-objects/question.spec.ts
@@ -5,6 +5,11 @@ import { Question } from './question'
 describe('Question Value Object', () => {
   test('Should return InvalidQuestionError if question not provided', () => {
     const sut = Question.create('')
-    expect(sut).toEqual(left(new InvalidQuestionError()))
+    expect(sut).toEqual(left(new InvalidQuestionError('Question not provided')))
+  })
+
+  test('Should return InvalidQuestionError if question contains less than 7 characters', () => {
+    const sut = Question.create('1234')
+    expect(sut).toEqual(left(new InvalidQuestionError('Contains less than 7 characters')))
   })
 })
