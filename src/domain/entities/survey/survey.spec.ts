2dd66a352ae2c73187720f81146957012f1c4e21
mlucasdev <lucasmsantos819@gmail.com>
2023-07-31 19:19:18 -0300

    test: ensure Survey return InvalidImageError if SurveyAnswer return this error

--- src/domain/entities/survey/survey.spec.ts
+++ src/domain/entities/survey/survey.spec.ts
@@ -1,14 +1,31 @@
 import { left } from '../../../shared/either'
-import { InvalidQuestionError } from './errors'
+import { InvalidImageError, InvalidQuestionError } from './errors'
 import { Question } from './value-objects/question'
-import { Survey } from './survey'
+import { Survey, SurveyData } from './survey'
+import { SurveyAnswer } from './value-objects'
+
+const makeFakeSurveyData = (): SurveyData => ({
+  question: 'any_question',
+  answers: [{
+    image: 'http://valid-image-url.com',
+    answer: 'any_answer'
+  }]
+})
 
 describe('Survey Entity', () => {
-  test('Should return InvalidQuestionError if Question return a error', () => {
+  test('Should return InvalidQuestionError if Question return this error', () => {
     jest.spyOn(Question, 'create').mockReturnValueOnce(
       left(new InvalidQuestionError('any message'))
     )
-    const sut = Survey.create({ question: 'any question' })
+    const sut = Survey.create(makeFakeSurveyData())
     expect(sut).toEqual(left(new InvalidQuestionError('any message')))
+  })
+
+  test('Should return InvalidImageError if SurveyAnswer return this error', () => {
+    jest.spyOn(SurveyAnswer, 'create').mockReturnValueOnce(
+      left(new InvalidImageError('any message'))
+    )
+    const sut = Survey.create(makeFakeSurveyData())
+    expect(sut).toEqual(left(new InvalidImageError('any message')))
   })
 })
