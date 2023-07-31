7f4837c4716d80b550f0ed89a392f34c7fe72ed3
mlucasdev <lucasmsantos819@gmail.com>
2023-07-31 11:02:03 -0300

    test: ensure Image return an Image if data is a valid link

--- src/domain/entities/survey/value-objects/image.spec.ts
+++ src/domain/entities/survey/value-objects/image.spec.ts
@@ -1,10 +1,15 @@
-import { left } from '../../../../shared/either'
+import { left, right } from '../../../../shared/either'
 import { InvalidImageError } from '../errors'
 import { Image } from './image'
 
 describe('Image ValueObject', () => {
-  test("Should return an InvalidImageError if the image don't have link format - 1", () => {
+  test('Should return an InvalidImageError if the image was not provided', () => {
     const sut = Image.create('')
+    expect(sut).toEqual(left(new InvalidImageError('was not provided')))
+  })
+
+  test("Should return an InvalidImageError if the image don't have link format - 1", () => {
+    const sut = Image.create('url-invalid')
     expect(sut).toEqual(left(new InvalidImageError("don't have link format")))
   })
 
@@ -21,5 +26,30 @@ describe('Image ValueObject', () => {
   test("Should return an InvalidImageError if the image don't have link format - 4", () => {
     const sut = Image.create('htt/url')
     expect(sut).toEqual(left(new InvalidImageError("don't have link format")))
+  })
+
+  test('Should return Image if image is valid - 1', () => {
+    const sut = Image.create('www.google.com')
+    expect(sut).toEqual(right({ image: 'www.google.com' }))
+  })
+
+  test('Should return Image if image is valid - 2', () => {
+    const sut = Image.create('https://www.google.com')
+    expect(sut).toEqual(right({ image: 'https://www.google.com' }))
+  })
+
+  test('Should return Image if image is valid - 3', () => {
+    const sut = Image.create('https://github.com/codedbylucas')
+    expect(sut).toEqual(right({ image: 'https://github.com/codedbylucas' }))
+  })
+
+  test('Should return Image if image is valid - 4', () => {
+    const sut = Image.create('https://github.com/codedbylucas?tab=repositories')
+    expect(sut).toEqual(right({ image: 'https://github.com/codedbylucas?tab=repositories' }))
+  })
+
+  test('Should return Image if image is valid - 5', () => {
+    const sut = Image.create('https://github.com/codedbylucas/clean-node-api')
+    expect(sut).toEqual(right({ image: 'https://github.com/codedbylucas/clean-node-api' }))
   })
 })
