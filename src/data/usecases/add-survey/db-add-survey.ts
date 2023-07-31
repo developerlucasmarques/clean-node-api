61adefdf97930049fa4c41915d477fa72eb85a3b
mlucasdev <lucasmsantos819@gmail.com>
2023-07-31 19:39:20 -0300

    feat: ensure DbAddSurvey return an Error if Survey creation fails

--- src/data/usecases/add-survey/db-add-survey.ts
+++ src/data/usecases/add-survey/db-add-survey.ts
@@ -1,10 +1,17 @@
-import { AddSurvey, AddSurveyData } from '../../../domain/usecases/add-survey'
+import { Survey } from '../../../domain/entities/survey'
+import { AddSurvey, AddSurveyData, AddSurveyResponse } from '../../../domain/usecases/add-survey'
+import { left, right } from '../../../shared/either'
 import { AddSurveyRepository } from '../../protocols/db/survey/add-survey-repository'
 
 export class DbAddSurvey implements AddSurvey {
   constructor (private readonly addSurveyRepository: AddSurveyRepository) {}
 
-  async add (data: AddSurveyData): Promise<void> {
+  async add (data: AddSurveyData): Promise<AddSurveyResponse> {
+    const survey = Survey.create(data)
+    if (survey.isLeft()) {
+      return left(survey.value)
+    }
     await this.addSurveyRepository.add(data)
+    return right(null)
   }
 }
