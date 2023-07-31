c80ee83552e62f6771d49377e2fc338242b1ae24
mlucasdev <lucasmsantos819@gmail.com>
2023-07-31 09:58:06 -0300

    test: ensure Answer return InvalidAnswerError if answer contains less than 3 characters

--- src/domain/entities/survey/value-objects/answer.spec.ts
+++ src/domain/entities/survey/value-objects/answer.spec.ts
@@ -7,4 +7,9 @@ describe('Answer ValueObject', () => {
     const sut = Answer.create('')
     expect(sut).toEqual(left(new InvalidAnswerError('not provided')))
   })
+
+  test('Should return InvalidAnswerError if answer contains less than 3 characters', () => {
+    const sut = Answer.create('ab')
+    expect(sut).toEqual(left(new InvalidAnswerError('contains less than 3 characters')))
+  })
 })
