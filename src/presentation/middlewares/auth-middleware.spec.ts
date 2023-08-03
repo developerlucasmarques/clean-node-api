6572151f92d2ef7d7cb68db635aa9e94d0e24a90
mlucasdev <lucasmsantos819@gmail.com>
2023-08-03 17:13:19 -0300

    test: ensure AuthMiddleware calls LoadAccountByToken with correct accessToken

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,11 +1,52 @@
 import { unauthorized } from '../helpers/http/http-helper'
 import { AccessDeniedError } from '../errors'
 import { AuthMiddleware } from './auth-middleware'
+import { LoadAccountByToken, LoadAccountByTokenData } from '../../domain/usecases'
+import { AccountModel } from '../../domain/models/account'
+
+const makeLoadAccountByToken = (): LoadAccountByToken => {
+  class LoadAccountByTokenStub implements LoadAccountByToken {
+    async load (data: LoadAccountByTokenData): Promise<AccountModel> {
+      return await Promise.resolve(makeFakeAccountModel())
+    }
+  }
+  return new LoadAccountByTokenStub()
+}
+
+const makeFakeAccountModel = (): AccountModel => ({
+  id: 'any_id',
+  name: 'any name',
+  email: 'any_email@mail.com',
+  password: 'hashed_password'
+})
+
+interface SutTypes {
+  sut: AuthMiddleware
+  loadAccountByTokenStub: LoadAccountByToken
+}
+
+const makeSut = (): SutTypes => {
+  const loadAccountByTokenStub = makeLoadAccountByToken()
+  const sut = new AuthMiddleware(loadAccountByTokenStub)
+  return {
+    loadAccountByTokenStub,
+    sut
+  }
+}
 
 describe('Auth Middleware', () => {
   test('Should return 401 if no x-access-token exists in headers', async () => {
-    const sut = new AuthMiddleware()
+    const { sut } = makeSut()
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(unauthorized(new AccessDeniedError()))
+  })
+
+  test('Should call LoadAccountByToken with correct accessToken', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut()
+    const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load')
+    await sut.handle({
+      headers: { 'x-access-token': 'any_token' }
+    })
+    expect(loadSpy).toHaveBeenCalledWith('any_token')
   })
 })
