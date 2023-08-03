f304596118471707a14e70063292053d95eeb798
mlucasdev <lucasmsantos819@gmail.com>
2023-08-03 19:25:21 -0300

    test: ensure AuthMiddleware returns 401 if LoadAccountByToken return a InvalidTokenError

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,14 +1,16 @@
-import { unauthorized } from '../helpers/http/http-helper'
-import { AccessDeniedError } from '../errors'
-import { AuthMiddleware } from './auth-middleware'
-import { LoadAccountByToken, LoadAccountByTokenData } from '../../domain/usecases'
+import { InvalidTokenError } from '../../domain/errors'
 import { AccountModel } from '../../domain/models/account'
+import { LoadAccountByToken, LoadAccountByTokenData, LoadAccountByTokenResponse } from '../../domain/usecases'
+import { left, right } from '../../shared/either'
+import { AccessTokenNotInformedError } from '../errors'
+import { unauthorized } from '../helpers/http/http-helper'
 import { HttpRequest } from '../protocols'
+import { AuthMiddleware } from './auth-middleware'
 
 const makeLoadAccountByToken = (): LoadAccountByToken => {
   class LoadAccountByTokenStub implements LoadAccountByToken {
-    async load (data: LoadAccountByTokenData): Promise<AccountModel> {
-      return await Promise.resolve(makeFakeAccountModel())
+    async load (data: LoadAccountByTokenData): Promise<LoadAccountByTokenResponse> {
+      return await Promise.resolve(right(makeFakeAccountModel()))
     }
   }
   return new LoadAccountByTokenStub()
@@ -43,7 +45,7 @@ describe('Auth Middleware', () => {
   test('Should return 401 if no x-access-token exists in headers', async () => {
     const { sut } = makeSut()
     const httpResponse = await sut.handle({})
-    expect(httpResponse).toEqual(unauthorized(new AccessDeniedError()))
+    expect(httpResponse).toEqual(unauthorized(new AccessTokenNotInformedError()))
   })
 
   test('Should call LoadAccountByToken with correct accessToken', async () => {
@@ -51,5 +53,14 @@ describe('Auth Middleware', () => {
     const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load')
     await sut.handle(makeFakeRequest())
     expect(loadSpy).toHaveBeenCalledWith('any_token')
+  })
+
+  test('Should return 401 if LoadAccountByToken returns a InvalidTokenError', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut()
+    jest.spyOn(loadAccountByTokenStub, 'load').mockReturnValueOnce(
+      Promise.resolve(left(new InvalidTokenError()))
+    )
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(unauthorized(new InvalidTokenError()))
   })
 })
