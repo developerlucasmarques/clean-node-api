44b0d6ae617f7f459ec94fbfa2776ec7f44d6d1a
mlucasdev <lucasmsantos819@gmail.com>
2023-08-10 18:59:55 -0300

    fix: remove invalid export

--- src/presentation/controllers/access/login/index.ts
+++ src/presentation/controllers/access/login/index.ts
@@ -1,4 +1,3 @@
 export * from '../../../protocols'
-export * from '../../../../validation/protocols/email-validator'
 export * from '../../../../domain/usecases/authentication'
 export * from '../../../../domain/errors/authentication-error'
