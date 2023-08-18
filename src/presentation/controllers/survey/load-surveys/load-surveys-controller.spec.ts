cda8070299cdd02ce0da6d7f3a09bd62f62efeb3
mlucasdev <lucasmsantos819@gmail.com>
2023-08-18 17:28:36 -0300

    test: ensure LoadSurveysController return 204 if LoadSurveys is empty

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
@@ -1,6 +1,7 @@
 import { LoadSurveysController } from './load-surveys-controller'
 import { LoadSurveys } from '../../../../domain/usecases'
 import { SurveyModel } from '../../../../domain/models'
+import { noContent } from '../../../helpers/http/http-helper'
 
 const makeLoadSurveys = (): LoadSurveys => {
   class LoadSurveyStub implements LoadSurveys {
@@ -52,5 +53,14 @@ describe('LoadSurveys Controller', () => {
     const loadSpy = jest.spyOn(loadSurveysStub, 'load')
     await sut.handle({})
     expect(loadSpy).toHaveBeenCalled()
+  })
+
+  test('Should return 204 if LoadSurveys is empty', async () => {
+    const { sut, loadSurveysStub } = makeSut()
+    jest.spyOn(loadSurveysStub, 'load').mockReturnValueOnce(
+      Promise.resolve([])
+    )
+    const response = await sut.handle({})
+    expect(response).toEqual(noContent())
   })
 })
