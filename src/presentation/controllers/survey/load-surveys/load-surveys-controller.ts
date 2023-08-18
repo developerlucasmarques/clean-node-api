ed976bd330151171b5174aa15038f9d40515d0c1
mlucasdev <lucasmsantos819@gmail.com>
2023-08-18 17:30:21 -0300

    feat: ensure LoadSurveysController return 200 if LoadSurveys success

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -1,6 +1,6 @@
 import { LoadSurveys } from '../../../../domain/usecases'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
-import { noContent } from '../add-survey'
+import { noContent, ok } from '../add-survey'
 
 export class LoadSurveysController implements Controller {
   constructor (private readonly loadSurveys: LoadSurveys) {}
@@ -8,6 +8,6 @@ export class LoadSurveysController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const surveys = await this.loadSurveys.load()
     if (surveys.length === 0) return noContent()
-    return await Promise.resolve({ statusCode: 0, body: {} })
+    return ok(surveys)
   }
 }
