daf2cf906645c973195fe1a910adf8840032b98a
mlucasdev <lucasmsantos819@gmail.com>
2023-08-18 18:55:21 -0300

    feat: ensure SurveyMongoRepository load all surveys on success

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,10 +1,19 @@
+import { LoadSurveysRepository } from '../../../../data/protocols/db/survey'
 import { AddSurveyRepository } from '../../../../data/protocols/db/survey/add-survey-repository'
+import { SurveyModel } from '../../../../domain/models'
 import { AddSurveyData } from '../../../../domain/usecases/add-survey'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
   async add (surveyData: AddSurveyData): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('survey')
     await surveyCollection.insertOne(surveyData)
+  }
+
+  async loadAll (): Promise<SurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection('survey')
+    const surveys = await surveyCollection.find().toArray()
+    const surveysWithFormattedId: any[] = surveys.map((survey) => (MongoHelper.map(survey)))
+    return surveysWithFormattedId
   }
 }
