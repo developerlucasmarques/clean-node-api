c3ab443f0198d17d561bda453eaab339a824949d
mlucasdev <lucasmsantos819@gmail.com>
2023-08-18 17:28:31 -0300

    feat: ensure LoadSurveysController return 204 if LoadSurveys is empty

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -1,11 +1,13 @@
 import { LoadSurveys } from '../../../../domain/usecases'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
+import { noContent } from '../add-survey'
 
 export class LoadSurveysController implements Controller {
   constructor (private readonly loadSurveys: LoadSurveys) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadSurveys.load()
+    const surveys = await this.loadSurveys.load()
+    if (surveys.length === 0) return noContent()
     return await Promise.resolve({ statusCode: 0, body: {} })
   }
 }
