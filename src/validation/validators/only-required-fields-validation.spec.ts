bffa46274b4ec6e7d2b6b8b16528a9b26f338f07
mlucasdev <lucasmsantos819@gmail.com>
2023-08-19 14:16:33 -0300

    test: ensure OnlyRequiredFieldValidation returns null if received only required fields

--- src/validation/validators/only-required-fields-validation.spec.ts
+++ src/validation/validators/only-required-fields-validation.spec.ts
@@ -1,4 +1,4 @@
-import { left } from '../../shared/either'
+import { left, right } from '../../shared/either'
 import { UnnecessaryFieldError } from '../../presentation/errors/unnecessary-field-error'
 import { OnlyRequiredFieldsValidation } from './only-required-fields-validation'
 
@@ -11,5 +11,14 @@ describe('OnlyRequiredField Validation', () => {
       role: 'any_role'
     })
     expect(response).toEqual(left(new UnnecessaryFieldError('role')))
+  })
+
+  test('Should return null if received only required fields', () => {
+    const sut = new OnlyRequiredFieldsValidation(['name', 'email'])
+    const result = sut.validate({
+      name: 'any_name',
+      email: 'any_email@mail.com'
+    })
+    expect(result).toEqual(right(null))
   })
 })
