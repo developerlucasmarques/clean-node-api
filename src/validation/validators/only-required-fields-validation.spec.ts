693c340924b72429e35d56c502e6938914f217bd
mlucasdev <lucasmsantos819@gmail.com>
2023-08-19 14:17:52 -0300

    refactor: move sut creation to a factory method

--- src/validation/validators/only-required-fields-validation.spec.ts
+++ src/validation/validators/only-required-fields-validation.spec.ts
@@ -2,9 +2,13 @@ import { left, right } from '../../shared/either'
 import { UnnecessaryFieldError } from '../../presentation/errors/unnecessary-field-error'
 import { OnlyRequiredFieldsValidation } from './only-required-fields-validation'
 
+const makeSut = (): OnlyRequiredFieldsValidation => {
+  return new OnlyRequiredFieldsValidation(['name', 'email'])
+}
+
 describe('OnlyRequiredField Validation', () => {
   test('Should return UnnecessaryFieldError if received field unnecessary', () => {
-    const sut = new OnlyRequiredFieldsValidation(['name', 'email'])
+    const sut = makeSut()
     const response = sut.validate({
       name: 'any_name',
       email: 'any_email@mail.com',
@@ -14,7 +18,7 @@ describe('OnlyRequiredField Validation', () => {
   })
 
   test('Should return null if received only required fields', () => {
-    const sut = new OnlyRequiredFieldsValidation(['name', 'email'])
+    const sut = makeSut()
     const result = sut.validate({
       name: 'any_name',
       email: 'any_email@mail.com'
