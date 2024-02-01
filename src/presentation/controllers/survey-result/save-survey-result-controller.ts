b97e76cc2984b46c33dd2c0bd25a440b1b4db38a
codedbylucas <lucasmsantos819@gmail.com>
2024-02-01 17:55:35 -0300

    feat: ensure SaveSurveyResultController call SaveSurveyResult with correct values

--- src/presentation/controllers/survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result-controller.ts
@@ -1,9 +1,13 @@
+import { SaveSurveyResult } from '@/domain/contracts'
 import { Controller, HttpRequest, HttpResponse, Validation } from '@/presentation/contracts'
 import { badRequest, serverError } from '@/presentation/helpers/http-helper'
 import { SaveSurveyResultDataController } from '@/presentation/types'
 
 export class SaveSurveyResultController implements Controller {
-  constructor (private readonly validation: Validation<SaveSurveyResultDataController>) {}
+  constructor (
+    private readonly validation: Validation<SaveSurveyResultDataController>,
+    private readonly saveSurveyResult: SaveSurveyResult
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
@@ -11,6 +15,10 @@ export class SaveSurveyResultController implements Controller {
       if (validationResult.isLeft()) {
         return badRequest(validationResult.value)
       }
+      const { answer } = httpRequest.body
+      const { surveyId } = httpRequest.params
+      const { accountId } = httpRequest.headers
+      await this.saveSurveyResult.save({ answer, surveyId, accountId, date: new Date() })
       return { statusCode: 9, body: '' }
     } catch (error: any) {
       return serverError(error)
