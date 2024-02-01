27c94f97567d3ecc497df70f3ce198a7e18cc7c4
codedbylucas <lucasmsantos819@gmail.com>
2024-02-01 17:41:24 -0300

    feat: ensure SaveSurveyResultController return 400 if Validation fails

--- src/presentation/controllers/survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result-controller.ts
@@ -1,11 +1,15 @@
 import { Controller, HttpRequest, HttpResponse, Validation } from '@/presentation/contracts'
+import { badRequest } from '@/presentation/helpers/http-helper'
 import { SaveSurveyResultDataController } from '@/presentation/types'
 
 export class SaveSurveyResultController implements Controller {
   constructor (private readonly validation: Validation<SaveSurveyResultDataController>) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.validation.validate(httpRequest.body)
+    const validationResult = this.validation.validate(httpRequest.body)
+    if (validationResult.isLeft()) {
+      return badRequest(validationResult.value)
+    }
     return { statusCode: 9, body: '' }
   }
 }
