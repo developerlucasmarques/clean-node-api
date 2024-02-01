0fc61c82e47ca08d46292a82aec7763cc67275f0
codedbylucas <lucasmsantos819@gmail.com>
2024-02-01 17:44:03 -0300

    feat: ensure SaveSurveyResultController return 500 if Validation throws

--- src/presentation/controllers/survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result-controller.ts
@@ -1,15 +1,19 @@
 import { Controller, HttpRequest, HttpResponse, Validation } from '@/presentation/contracts'
-import { badRequest } from '@/presentation/helpers/http-helper'
+import { badRequest, serverError } from '@/presentation/helpers/http-helper'
 import { SaveSurveyResultDataController } from '@/presentation/types'
 
 export class SaveSurveyResultController implements Controller {
   constructor (private readonly validation: Validation<SaveSurveyResultDataController>) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const validationResult = this.validation.validate(httpRequest.body)
-    if (validationResult.isLeft()) {
-      return badRequest(validationResult.value)
+    try {
+      const validationResult = this.validation.validate(httpRequest.body)
+      if (validationResult.isLeft()) {
+        return badRequest(validationResult.value)
+      }
+      return { statusCode: 9, body: '' }
+    } catch (error: any) {
+      return serverError(error)
     }
-    return { statusCode: 9, body: '' }
   }
 }
